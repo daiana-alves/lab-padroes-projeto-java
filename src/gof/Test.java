dd3dda207df2a8453bdb8931c98f3937d62dffdc
Daiana Alves <daianaalves93@outlook.com.br>
2022-05-30 23:38:30 -0300

    aula padrões de projetos dio

---
+++ src/gof/Test.java
@@ -0,0 +1,49 @@
+package gof;
+
+import gof.facade.Facade;
+import gof.singleton.SingletonEager;
+import gof.singleton.SingletonLazy;
+import gof.singleton.SingletonLazyHolder;
+import gof.strategy.*;
+
+public class Test {
+    public static void main(String[] args) {
+        SingletonLazy lazy = SingletonLazy.getInstance();
+        System.out.println(lazy);
+        lazy = SingletonLazy.getInstance();
+        System.out.println(lazy);
+
+        SingletonEager eager = SingletonEager.getInstance();
+        System.out.println(eager);
+        eager = SingletonEager.getInstance();
+        System.out.println(eager);
+
+        SingletonLazyHolder lazyHolder = SingletonLazyHolder.getInstancia();
+        System.out.println(lazyHolder);
+        lazyHolder = SingletonLazyHolder.getInstancia();
+        System.out.println(lazyHolder);
+
+        // Strategy
+
+        Comportamento defensivo = new ComportamentoDefensivo();
+        Comportamento normal = new ComportamentoNormal();
+        Comportamento agressivo = new ComportamentoAgressivo();
+
+        Robo robo = new Robo();
+        robo.setComportamento(normal);
+        robo.mover();
+        robo.mover();
+        robo.setComportamento(defensivo);
+        robo.mover();
+        robo.setComportamento(agressivo);
+        robo.mover();
+        robo.mover();
+        robo.mover();
+
+        // Facade
+
+        Facade facade = new Facade();
+        facade.migrarCliente("Venilton", "14801788");
+
+    }
+}
